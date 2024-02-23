55f552dc265984476147d7f9e8698bbb3ae546e7
snehitshh <snehitsh07@gmail.com>
2024-02-23 14:54:58 +0530

    stock bu and sell naive solution

--- arrays/stockBuySell1.cpp
+++ arrays/stockBuySell1.cpp
@@ -1,14 +1,13 @@
 #include<iostream>
 using namespace std;
 int maxProfit(int price[],int start,int end){
-if(end<=start){
+if(end<=start)
     return 0;
-}
 int profit=0;
 for(int i=start;i<end;i++){
     for(int j=i+1;j<=end;j++){
         if(price[j]>price[i]){
-            int curr_profit=(price[j]-price[i])+maxProfit(price,start,i-1)+maxProfit(price,j+1,end);
+            int curr_profit=(price[j]-price[i])+maxProfit(price,start,i-1)+maxProfit(price,j+1,end);  //checking all cases i.e on the left of i and on the right of j
             profit=max(profit,curr_profit);
         }
     }
@@ -17,7 +16,7 @@ return profit;
 }
 int main()
 {
-    int start,end;
-    int price
-
+    int start=0,end=4;
+    int price[5]={1,4,1,9,3};
+    maxProfit(price,start,end);
 }
