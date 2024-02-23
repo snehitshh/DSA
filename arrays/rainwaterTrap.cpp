e8c7ad12daf8dd79b98bb8d0af134cc739affc91
snehitshh <snehitsh07@gmail.com>
2024-02-23 22:50:04 +0530

    rainwater-comments to understand

--- arrays/rainwaterTrap.cpp
+++ arrays/rainwaterTrap.cpp
@@ -8,11 +8,11 @@ for(int i=1;i<n;i++){
 }
 right[n-1]=building[n-1];
 for(int i=n-2;i>=0;i--){
-    right[i]=max(right[1+i],building[i]);
+    right[i]=max(right[1+i],building[i]);//finding the building with the maximum height on right of any element
 }
 int ans=0;
 for(int i=0;i<n;i++){
-    ans+=(min(right[i],left[i])-building[i]);
+    ans+=(min(right[i],left[i])-building[i]);// the building which is more smaller(comparing left and right)-the current building-that will be the amount of water in that block
 }
 return ans;
 }
@@ -22,3 +22,4 @@ int n=5;
 int building[n]={5,0,6,2,3};
 cout<<rainWater(building,n);
 }
+
